3dbb777a22b49d6794b719a1952d8afe1b9dacf9
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-02-15 19:25:00 -0500

    feat: standard error classes

---
+++ packages/core/tests/errors.test.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Tests for the Hfs class.
+ * @author Nicholas C. Zakas
+ */
+/* global it, describe */
+
+//------------------------------------------------------------------------------
+// Imports
+//------------------------------------------------------------------------------
+
+import {
+	PermissionError,
+	DirectoryError,
+	NotFoundError,
+} from "../src/errors.js";
+import assert from "node:assert";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+describe("Errors", () => {
+	describe("NotFoundError", () => {
+		it("should create an instance with the correct message", () => {
+			const error = new NotFoundError("foo.txt");
+			assert.strictEqual(
+				error.message,
+				"ENOENT: No such file or directory, foo.txt",
+			);
+		});
+	});
+
+	describe("PermissionError", () => {
+		it("should create an instance with the correct message", () => {
+			const error = new PermissionError("foo.txt");
+			assert.strictEqual(
+				error.message,
+				"EPERM: Operation not permitted, foo.txt",
+			);
+		});
+	});
+
+	describe("DirectoryError", () => {
+		it("should create an instance with the correct message", () => {
+			const error = new DirectoryError("foo.txt");
+			assert.strictEqual(
+				error.message,
+				"EISDIR: Illegal operation on a directory, foo.txt",
+			);
+		});
+	});
+});
